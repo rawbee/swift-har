8aba93f730f38f9c89b642e91cccb8ced8412c31
Joshua Peek <josh@joshpeek.com>
2020-01-19 12:15:13 -0800

    Add copyright comment

--- Sources/HAR/HAR.swift
+++ Sources/HAR/HAR.swift
@@ -1,3 +1,11 @@
+//
+//  HAR.swift
+//  HAR
+//
+//  Created by Joshua Peek on 1/19/20.
+//  Copyright © 2020 Joshua Peek. All rights reserved.
+//
+
 struct HAR {
     var text = "Hello, World!"
 }
