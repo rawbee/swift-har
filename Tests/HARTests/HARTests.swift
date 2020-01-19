1c411552bf9bca17e9c392a53e497725c399403c
Joshua Peek <josh@joshpeek.com>
2020-01-19 12:58:57 -0800

    Add test fixture harness

--- Tests/HARTests/HARTests.swift
+++ Tests/HARTests/HARTests.swift
@@ -2,14 +2,30 @@
 import XCTest
 
 final class HARTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(HAR().text, "Hello, World!")
+    func testCodable() {
+        XCTAssertEqual(fixtures.count, 1)
+    }
+
+    var fixtureURL: URL {
+        var url = URL(fileURLWithPath: #file)
+        url.appendPathComponent("../../Fixtures")
+        url.standardize()
+        return url
+    }
+
+    var fixtures: [String: Data] {
+        var fixtures: [String: Data] = [:]
+        for name in try! FileManager.default.contentsOfDirectory(atPath: fixtureURL.path) {
+            fixtures[name] = fixture(name: name)
+        }
+        return fixtures
+    }
+
+    func fixture(name: String) -> Data {
+        try! Data(contentsOf: fixtureURL.appendingPathComponent(name))
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testCodable", testCodable),
     ]
 }
