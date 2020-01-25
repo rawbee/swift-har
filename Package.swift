261df75d990ea1436dd0e46ba571e7666ef27cd9
Joshua Peek <josh@joshpeek.com>
2020-01-24 23:25:52 -0800

    Revert "Target latest SDKs"
    This reverts commit aaab392cf96590663498b7c2f9e4af282799d8f0.

--- Package.swift
+++ Package.swift
@@ -4,12 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "HAR",
-    platforms: [
-        .macOS(.v10_15),
-        .tvOS(.v13),
-        .iOS(.v13),
-        .watchOS(.v6),
-    ],
     products: [
         .library(
             name: "HAR",
