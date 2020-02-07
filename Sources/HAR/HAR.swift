98ee7c387a303e53c3f94c366a1eecdb25952d27
Joshua Peek <josh@joshpeek.com>
2020-02-06 22:00:16 -0800

    This won't be useful

--- Sources/HAR/HAR.swift
+++ Sources/HAR/HAR.swift
@@ -627,8 +627,6 @@ extension HAR.Page: CustomDebugStringConvertible {
     }
 }
 
-extension HAR.Page: Identifiable {}
-
 extension HAR.Page: Codable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: Self.CodingKeys.self)
