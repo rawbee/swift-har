0f1c671971634cf5e8231a14fbab189e95958532
Joshua Peek <josh@joshpeek.com>
2020-02-26 17:43:02 -0800

    Add Entry to URL message

--- Sources/HAR/HAR.swift
+++ Sources/HAR/HAR.swift
@@ -792,6 +792,19 @@ extension HAR.Entry {
 }
 
 extension HAR.Entry {
+    // MARK: Instance Methods
+
+    /// Create Foundation URL request and response for archived entry.
+    public func toURLMessage() -> (request: URLRequest, response: HTTPURLResponse, data: Data) {
+        (
+            request: URLRequest(request: request),
+            response: HTTPURLResponse(url: request.url, response: response),
+            data: response.content.data
+        )
+    }
+}
+
+extension HAR.Entry {
     // MARK: Recording an Entry
 
     /// Perform URL Request and create HTTP archive Entry of the request and response.
--- Tests/HARTests/EntriesTests.swift
+++ Tests/HARTests/EntriesTests.swift
@@ -1,4 +1,14 @@
 @testable import HAR
 import XCTest
 
-final class EntriesTests: XCTestCase {}
+final class EntriesTests: XCTestCase {
+    func testURLMessage() throws {
+        let har = try HAR(data: XCTUnwrap(fixtureData["Safari example.com.har"]))
+        let harEntry = try XCTUnwrap(har.log.entries.first)
+        let urlMessage = harEntry.toURLMessage()
+
+        XCTAssertEqual(urlMessage.request.httpMethod, "GET")
+        XCTAssertEqual(urlMessage.response.statusCode, 200)
+        XCTAssertEqual(urlMessage.data.count, 1256)
+    }
+}
