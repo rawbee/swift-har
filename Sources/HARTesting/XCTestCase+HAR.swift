16f0083a1bf7c51686bfe85cf46e75c5f008eb5a
Joshua Peek <josh@joshpeek.com>
2020-04-14 21:46:58 -0700

    Import FoundationNetworking

--- Sources/HARTesting/XCTestCase+HAR.swift
+++ Sources/HARTesting/XCTestCase+HAR.swift
@@ -1,6 +1,10 @@
 import HAR
 import XCTest
 
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
 extension XCTestCase {
     public func recordNetworkingAttachment() -> URLSession {
         addTeardownBlock { self.add(HAR.TraceURLProtocol.tearDown().attachment) }
